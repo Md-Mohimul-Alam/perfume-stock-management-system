c896408ea8f3bdfdb155d3a1c8fa6376dac7a64d
Md-Mohimul-Alam <mohimreza1234@gmail.com>
2026-07-01 21:20:33 +0600

    back api

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,7 +11,7 @@ const app = express();
 
 // ✅ CORS configuration
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:5173','https://perfume-stock-management-system-545.vercel.app',);
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
