c960898311a464b4a997a8925062a2fbbfaf1d82
Md-Mohimul-Alam <mohimreza1234@gmail.com>
2026-08-10 14:45:46 +0600

    server update

--- backend/src/app.js
+++ backend/src/app.js
@@ -28,7 +28,7 @@ app.use(cors({
     }
   },
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],  // ← add PATCH
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
